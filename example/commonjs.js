b147174e07cb51a3e62a17150593817529851b9a
Nathachai Thongniran <inid3a@gmail.com>
2023-08-27 23:56:07 +0700

    docs(example): add 2 examples files: commonjs.js, es6.ts

---
+++ example/commonjs.js
@@ -0,0 +1,7 @@
+const { encode } = require('../src')
+
+const nRows = 30
+const nHrs = 24
+const hrs = [...Array(nRows).keys()].map(item => item % nHrs)
+const encodedHrs = encode(hrs, nHrs)
+console.log('encodedHrs', encodedHrs)
