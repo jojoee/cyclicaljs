76fb01d115a311c9348156f59c2e8e3768c2e37c
Nathachai Thongniran <inid3a@gmail.com>
2023-08-28 08:23:38 +0700

    docs(example): update 2 example files

--- example/commonjs.js
+++ example/commonjs.js
@@ -1,7 +1,13 @@
 const { encode } = require('../src')
 
-const nRows = 30
-const nHrs = 24
-const hrs = [...Array(nRows).keys()].map(item => item % nHrs)
-const encodedHrs = encode(hrs, nHrs)
-console.log('encodedHrs', encodedHrs)
+// example1
+const nItems1 = 24
+const items1 = Array.from({ length: 16 }, (_, i) => i).map(item => item % nItems1)
+const encodedItems1 = encode(items1, nItems1)
+console.log('encodedItems1', encodedItems1)
+
+// example2
+const nItems2 = 52
+const items2 = Array.from({ length: 60 }, (_, i) => i).map(item => item % nItems2)
+const encodedItems2 = encode(items2, nItems2)
+console.log('encodedItems2', encodedItems2)
